e678ac719e14e385ce69474e620ee1a8152e8fbc
Rene An <auj0827@gmail.com>
2020-04-21 12:56:17 -0400

    Reset for 2nd

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,49 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    private static GameManager instance = null;
-
-    public int population = 1000000000;
-    public int doller = 0;
-    public int playerDamage = 10;
-    public int followerDamage = 0;
-
-    private void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-        if(null == instance)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(this.gameObject);
-        }
+        
     }
 
-    public int getPlayerDamage()
+    // Update is called once per frame
+    void Update()
     {
-        return playerDamage;
-    }
-    public void setPlayerDamage(int addDamage)
-    {
-        playerDamage += addDamage;
-    }
-
-    public static GameManager Instance
-    {
-        get
-        {
-            if (null == instance)
-            {
-                return null;
-            }
-            return instance;
-        }
+        
     }
 }
-
