99f55cb8285c96793ff0703176aa494d3cf7c55e
Rene An <auj0827@gmail.com>
2020-04-21 13:14:37 -0400

    Teddy
    Game touch func and basic UI

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,18 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static GameManager instance = null;
+
+    public int population = 1000000000;
+    public int doller = 0;
+    public int playerDamage = 10;
+    public int followerDamage = 0;
+
+    private void Awake()
     {
-        
+        if(null == instance)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public int getPlayerDamage()
     {
-        
+        return playerDamage;
+    }
+    public void setPlayerDamage(int addDamage)
+    {
+        playerDamage += addDamage;
+    }
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (null == instance)
+            {
+                return null;
+            }
+            return instance;
+        }
     }
 }
+
